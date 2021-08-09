4353e82eae476425021e301c6ff45de04c76a8f4
ehddn453 <86578755+ehddn453@users.noreply.github.com>
2021-08-09 16:04:29 +0900

    v2.2

--- js/index.js
+++ js/index.js
@@ -36,26 +36,41 @@ const elMain1Popup = document.querySelector('.main1_popup'),
 //오른쪽 위 빨간색 팝업창
 setTimeout(function(){
     elMain1Popup.classList.add('show');
-},1500);
+},1000);
 //text
 setTimeout(function(){
     for(let i=0; i<elMain1H2.length; i++){
         elMain1H2[i].classList.add('show');
     }
     elMain1P.classList.add('show');
-},1000);
+},400);
 
-//main3 지도 나타나기
+//main3
 const elMain3 = document.querySelector('.main3'),
-      elMain3Right = document.querySelector('.main3 .main3_right');
+      elMain3Right = document.querySelector('.main3 .main3_right'),
+      elMain3H2 = document.querySelector('.main3 h2');
 window.addEventListener('scroll',function(){
+    //main3 지도 나타나기
     let elMain3Height = elMain3.offsetTop - window.innerHeight;
     if( elMain3Height <= window.scrollY - 600){
         elMain3Right.classList.add('show')
     }else{
         elMain3Right.classList.remove('show')
-    }
+    };
+    //About us 스크롤에 따라 움직이기
+    let aboutHeight = (elMain3H2.offsetTop - window.innerHeight) <= window.scrollY - 300
+    let idx = elMain3H2.offsetTop - this.scrollY
+    
+    if(aboutHeight){
+        elMain3H2.style.transform = `translate(${idx}px,45%)`
+        if(idx<=0){
+            elMain3H2.style.transform = `translate(0,45%)`
+        };
+    };
+
 });
+// idx = 오브젝트위치 - 스크롤값
+       
 
 //main4
 const elMain4 = document.querySelector('.main4'),
